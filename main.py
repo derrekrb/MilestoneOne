3a250dc4e78903ac577f12aaab0478bc5612513f
tannererekson <78454938+tannererekson@users.noreply.github.com>
2021-02-11 11:14:51 -0700

    Testing out github
    this is not complete im just testing out the branches

--- main.py
+++ main.py
@@ -17,11 +17,12 @@ def read():
 def write():
     return
 
-def load():
-    return
+def load(location, accumulator):
+    accumulator = memory[location]
+    return accumulator
 
-def store():
-    return
+def store(location, accumulator):
+    memory[location]= accumulator
 
 
 
@@ -39,8 +40,27 @@ def main():
         memory.append(entry_command)
         program_counter += 1
 
-    #for i in memory:
-        #print(i)
+    for i in memory:
+        i = str(i)
+        op = int(i[0:2])
+        
+        if op == 10:
+            print("read")
+        if op == 11:
+            print("write")
+        if op == 20:
+            print("load")
+        if op == 21:
+            print("store")
+        if op == 30:
+            print("add")
+        if op == 31:
+            print("subtract")
+        if op == 32:
+            print("divide")
+        if op == 33:
+            print("multiply")
+        
     
 
 
