13d7710f61cca4ad8a99ae7e44d2ee45d5586918
Matthew Palmer <palmermatthew645@gmail.com>
2021-02-11 13:39:21 -0700

    Started writing read() function and provided some minor formatting to the main.py

--- main.py
+++ main.py
@@ -5,43 +5,46 @@ def add(number, accumulator):
 def subtract():
     return
 
+
 def multiply():
     return
+
 
 def divide():
     return
 
-def read():
+
+def read(memoryDestination):
+    """Reads a word from the keyboard into a specific location in memory"""
+    userInput = input("Enter an integer: ")
+    memory[memoryDestination] = userInput
+
+
+def write(memoryLocation):
     return
 
-def write():
-    return
 
 def load():
     return
+
 
 def store():
     return
 
 
-
-
 def main():
     program_counter = 0
     memory = []
-    entry_command = 1 
+    entry_command = 1
     accumulator = 0
-    
-    
-    
+
     while entry_command != -99999:
         entry_command = int((input(str(program_counter).zfill(2) + " ? ")))
         memory.append(entry_command)
         program_counter += 1
 
-    #for i in memory:
-        #print(i)
-    
+    # for i in memory:
+    # print(i)
 
 
 if __name__ == "__main__":
