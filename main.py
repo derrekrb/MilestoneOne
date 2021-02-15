d24384aa4c1e254c556930b8529c5ad7c8d6dfc8
Matthew Palmer <palmermatthew645@gmail.com>
2021-02-15 13:26:30 -0700

    Worked on read() function and allocated memory of 100.  Also added some checks for input error within main() and made some formatting corrections

--- main.py
+++ main.py
@@ -5,38 +5,38 @@ Tanner Erekson
 Derrek Buttars
 Matthew Palmer
 """
-memory = []
+memory = ["+0000"]*100
 
 
 def add(memory_location, accumulator):
-    '''Adds a number from a specific locaiton in memory to the number in the accumulator.'''
-    
+    """Adds a number from a specific locaiton in memory to the number in the accumulator."""
+
     memory_value = memory[memory_location]
     accumulator += memory_value
     return accumulator
 
 
 def subtract(memory_location, accumulator):
-    '''Subtracts a number from a specific location in memory from the number in the accumulator.'''
-    
+    """Subtracts a number from a specific location in memory from the number in the accumulator."""
+
     memory_value = memory[memory_location]
     accumulator -= memory_value
     return accumulator
 
 
 def multiply(memory_location, accumulator):
-    '''Multiplies a number from a specific memory location to the number in the accumulator
-    and returns the accumulator'''
-    
+    """Multiplies a number from a specific memory location to the number in the accumulator
+    and returns the accumulator"""
+
     memory_value = memory[memory_location]
     accumulator *= memory_value
     return accumulator
 
 
 def divide(memory_location, accumulator):
-    '''Divides the number in the accumulator by a number from a specific location in memory
-    and returns the accumulator.'''
-    
+    """Divides the number in the accumulator by a number from a specific location in memory
+    and returns the accumulator."""
+
     memory_value = memory[memory_location]
     accumulator /= memory_value
     return accumulator
@@ -45,14 +45,27 @@ def divide(memory_location, accumulator):
 def read(memory_location):
     """Asks the user for an integer and puts it into a specific location in memory"""
 
-    userInput = input("Enter an integer: ")
-    memory[memory_location] = userInput
-    return
+    userInput = input("Enter an integer between -9999 and 9999: ")
+    # if type(userInput) != int:
+    #     print("must be an integer")
+    try:
+        userInput = int(userInput)
+    except ValueError:
+        print("Must be an int")
+    except TypeError:
+        print("Must be an int")
+    else:
+        if userInput > 9999 or userInput < -9999:
+            print("must be an integer between -9999 and +9999")
+
+        else:
+            memory[memory_location] = userInput
+            return
 
 
 def write(memory_location):
-    '''Prints the contents of the given memory location to the screen'''
-    
+    """Prints the contents of the given memory location to the screen"""
+
     print(memory[memory_location])
     return
 
@@ -62,51 +75,60 @@ def load(memory_location, accumulator):
     accumulator = memory[memory_location]
     return accumulator
 
+
 def store(memory_location, accumulator):
     """ Will take whatever is in the accumulator and will store it in the given location """
-    memory[memory_location]= accumulator
+    memory[memory_location] = accumulator
     return
-
 
 
 def main():
     entry_command = 1
     program_counter = 0
     accumulator = 0
-    
-    while entry_command != -99999:
-        entry_command = int((input(str(program_counter).zfill(2) + " ? ")))
+    print(
+        "~~ Instructions given in the UVsim must be in the format of +0000.  Ex. +1001 is a valid instruction ~~"
+    )
+
+    while entry_command != "-99999":
+        entry_command = input(str(program_counter).zfill(2) + " ? ")
         memory.append(entry_command)
         program_counter += 1
 
-
     for i in memory:
         i = str(i)
-        op = int(i[0:2])
-        memory_location = int(i[2:4])
-        
-        if op == 10:
-            read(memory_location)
-        if op == 11:
-            write(memory_location)
-        if op == 20:
-            load(memory_location, accumulator)
-        if op == 21:
-            store(memory_location, accumulator)
-        if op == 30:
-            add(memory_location, accumulator)
-        if op == 31:
-            subtract(memory_location, accumulator)
-        if op == 32:
-            divide(memory_location, accumulator)
-        if op == 33:
-            multiply(memory_location, accumulator)
-        
-    
+
+        if len(i) != 5:  # Checks if length of instruction is correct
+            if i != "-99999":
+                print(f"{i} is not a valid instruction")
+
+        elif i[0] != "+":  # Checks if instruction contain a + (Except if -99999)
+            if i != "-99999":
+                print(f"{i} is not a valid instruction")
+
+        else:
+            op = int(i[1:3])
+            memory_location = int(i[3:5])
+
+            if op == 10:
+                read(memory_location)
+            if op == 11:
+                write(memory_location)
+            if op == 20:
+                load(memory_location, accumulator)
+            if op == 21:
+                store(memory_location, accumulator)
+            if op == 30:
+                add(memory_location, accumulator)
+            if op == 31:
+                subtract(memory_location, accumulator)
+            if op == 32:
+                divide(memory_location, accumulator)
+            if op == 33:
+                multiply(memory_location, accumulator)
 
     # for i in memory:
-    # print(i)
-
+    #     print(i)
 
 
 if __name__ == "__main__":
